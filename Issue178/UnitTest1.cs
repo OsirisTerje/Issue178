dc2c553a2cfbad1311be83a399efa4558f476993
Saverio Cisternino <workgroupengineering@outlook.com>
2019-05-13 14:56:16 +0200

    Add project files.

---
+++ Issue178/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Issue178
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
